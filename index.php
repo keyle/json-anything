154b12a09f28f015dad54d798a655e5088a7af67
keyle <keyle@capsule.org>
2011-07-23 23:58:21 +1000

    added | parsing

--- index.php
+++ index.php
@@ -21,6 +21,11 @@ if(!$_GET['s1']) die("give at least 1 selector ie ...?s1='.class a'");
 $_GET['s1'] = filterNasty($_GET['s1']);
 $s1 = $_GET['s1'];
 
+$selector1 = explode('|', $s1);
+
+if(count($selector1) < 2)
+	$selector1[1] = null;
+
 $output = file_get_contents('http://'.$url) or die('Could not access file: $url');
 
 phpQuery::newDocument($output);
@@ -33,9 +38,11 @@ foreach($_GET as $name => $value)
 	print "$name : $value<br>";
 }
 
-$j = json_encode(filterThis($s1));
-echo $j;
+var_dump($selector1);
 
+//$j = json_encode(filterThis($selector1[0], $selector1[1]));
+//echo $j;
+var_dump(filterThis($selector1[0], $selector1[1]));
 
 function filterThis($selector, $attribute = null)
 {
@@ -43,7 +50,7 @@ function filterThis($selector, $attribute = null)
 
 	foreach(pq($selector) as $item)
 	{
-		$attribute == null ? array_push($arr, pq($item)->html()) : 	array_push($arr, pq($item)->attr('src'));
+		$attribute == null ? array_push($arr, pq($item)->html()) : 	array_push($arr, pq($item)->attr($attribute));
 	}
 
 	return $arr;

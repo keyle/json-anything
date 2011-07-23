697caff2f719cc559cb9c2b07f25280841938825
keyle <keyle@capsule.org>
2011-07-23 23:26:52 +1000

    initial commit

---
+++ index.php
@@ -0,0 +1,54 @@
+<pre>
+
+
+
+<?php
+//header('Cache-Control: no-cache, must-revalidate');
+//header('Expires: Mon, 26 Jul 1997 05:00:00 GMT');
+//header('Content-type: application/json');
+
+function filterNasty($stuff) { return filter_var(htmlspecialchars(pg_escape_string($stuff)), FILTER_SANITIZE_STRING, FILTER_FLAG_STRIP_HIGH); }
+
+require_once('phpQuery-onefile.php');
+
+if(!$_GET['url']) die('give a url param.');
+
+$_GET['url'] = filterNasty($_GET['url']);
+$url = $_GET['url'];
+
+if(!$_GET['s1']) die("give at least 1 selector ie ...?s1='.class a'");
+
+$_GET['s1'] = filterNasty($_GET['s1']);
+$s1 = $_GET['s1'];
+
+$output = file_get_contents('http://'.$url) or die('Could not access file: $url');
+
+phpQuery::newDocument($output);
+
+//echo pq('title')->html() . "\n";
+//echo pq('body')->find('img');
+
+foreach($_GET as $name => $value)
+{
+	print "$name : $value<br>";
+}
+
+$j = json_encode(filterThis($s1));
+echo $j;
+
+
+function filterThis($selector, $attribute = null)
+{
+	$arr = array();
+
+	foreach(pq($selector) as $item)
+	{
+		$attribute == null ? array_push($arr, pq($item)->html()) : 	array_push($arr, pq($item)->attr('src'));
+	}
+
+	return $arr;
+}
+
+?>
+
+

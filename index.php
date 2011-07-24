a0d67830541df81784b4e18827becc73773e904b
keyle <keyle@capsule.org>
2011-07-24 16:39:25 +1000

    added debug, cleaned up, more stable

--- index.php
+++ index.php
@@ -1,20 +1,22 @@
-<pre>
-
-
-
 <?php
-//header('Cache-Control: no-cache, must-revalidate');
-//header('Expires: Mon, 26 Jul 1997 05:00:00 GMT');
-//header('Content-type: application/json');
+header('Cache-Control: no-cache, must-revalidate');
+header('Expires: Mon, 26 Jul 1997 05:00:00 GMT');
+header('Content-type: application/json');
 
 function filterNasty($stuff) {
-	return filter_var(
-			htmlspecialchars(
-				pg_escape_string($stuff)
-			),
-			FILTER_SANITIZE_STRING, FILTER_FLAG_STRIP_HIGH
-		);
+	return filter_var
+	(
+		htmlspecialchars
+		(
+			pg_escape_string
+			(
+				strip_tags($stuff)
+			)
+		), FILTER_SANITIZE_STRING, FILTER_FLAG_STRIP_HIGH
+	);
 }
+
+ini_set('user_agent', 'Mozilla/5.0 (Windows NT 6.1; U; ru; rv:5.0.1.6) Gecko/20110501 Firefox/5.0.1 Firefox/5.0.1');
 
 require_once('phpQuery-onefile.php');
 
@@ -23,33 +25,93 @@ if(!$_GET['url']) die('give a url param.');
 $_GET['url'] = filterNasty($_GET['url']);
 $url = $_GET['url'];
 
-if(!$_GET['s1']) die("give at least 1 selector, ie. ...?s1=.class a");
+if(!$_GET['sel']) die("give at least 1 selector, ie. ...?sel=.class__a");
 
-$_GET['s1'] = filterNasty($_GET['s1']);
-$s1 = $_GET['s1'];
+$_GET['sel'] = filterNasty($_GET['sel']);
+$sel = $_GET['sel'];
 
-$selector1 = explode('|', $s1);
+$debug = isset($_GET['debug']) ? true : false;
 
-if(count($selector1) < 2)
-	$selector1[1] = null;
+$sel = replaceThisByThatInThat('__', ' ', $sel);
+$sel = replaceThisByThatInThat('%', '#', $sel);
 
-$output = file_get_contents('http://'.$url) or die('Could not access: ?url=...');
+$selectorsArr = explode(',', $sel);
+
+for ($i=0; $i < count($selectorsArr); $i++)
+{
+	$selectorsArr[$i] = explode('|', $selectorsArr[$i]);
+
+	if(count($selectorsArr[$i]) < 2)
+		$selectorsArr[$i][1] = null;
+}
+$output = file_get_contents($url) or die('Could not access: ?url=...');
 
 phpQuery::newDocument($output);
 
-//echo pq('title')->html() . "\n";
-//echo pq('body')->find('img');
 
-foreach($_GET as $name => $value)
+$mess = filterAll($selectorsArr);
+
+$clean = rearrange($mess);
+
+$output = new stdClass();
+$output->url = replaceThisByThatInThat('\\','',$url);
+$output->sel = $sel;
+
+$output->results = $clean;
+
+$encoded = json_encode($output);
+
+$encoded = replaceThisByThatInThat('\\/', '/', $encoded);
+
+
+if($debug)
 {
-	print "$name : $value<br>";
+	var_dump($output);
+}
+else echo( $encoded );
+
+
+function filterAll($selectors)
+{
+	$all = array();
+
+	foreach($selectors as $i => $oneSelector)
+	{
+		$all[$i] = (filterthis($oneSelector[0], $oneSelector[1]) );
+	}
+
+	return $all;
 }
 
-var_dump($selector1);
+// takes arrays[0][X] and arrays[1][X] to arrays[X][0,1]
+function rearrange($arrays)
+{
+	$result = array();
+	$temp = array();
 
-//$j = json_encode(filterThis($selector1[0], $selector1[1]));
-//echo $j;
-var_dump(filterThis($selector1[0], $selector1[1]));
+	foreach($arrays as $onearr)
+	{
+		$temp = array_merge($temp, $onearr);
+	}
+
+	//$temp = array_merge($arrays[0], $arrays[1]);
+
+	$nominal_length = count($arrays[0]);
+	$total_arrays = count($arrays);
+
+	for ($i=0; $i < $nominal_length; $i++)
+	{
+		for ($j=0; $j < $total_arrays; $j++)
+		{
+			try {
+				$result[$i][$j] = $temp[$i+($nominal_length*$j)];
+			} catch(Exception $e) {}
+		}
+	}
+
+	return $result;
+}
+
 
 function filterThis($selector, $attribute = null)
 {
@@ -57,12 +119,24 @@ function filterThis($selector, $attribute = null)
 
 	foreach(pq($selector) as $item)
 	{
-		$attribute == null ?
-		array_push($arr, pq($item)->html()) :
-		array_push($arr, pq($item)->attr($attribute));
+		if($attribute == null)
+		{
+			array_push($arr, rtrim(ltrim(pq($item)->text())));
+		}
+		else if($attribute == 'html')
+		{
+			array_push($arr, rtrim(ltrim(pq($item)->html())));
+		}
+		else
+			array_push($arr, pq($item)->attr($attribute));
 	}
 
 	return $arr;
+}
+
+function replaceThisByThatInThat($this, $byThat, $inThat)
+{
+	return str_replace($this, $byThat, $inThat);
 }
 
 ?>
